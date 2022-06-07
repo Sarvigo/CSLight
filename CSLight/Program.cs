4fa0c539ca513312f7135c1beafe4a4da44a6fe6
gorba <gorbachev.17@mail.ru>
2022-06-07 14:12:22 +0300

    Добавьте файлы проекта.

---
+++ CSLight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Мир!");
+            Console.ReadLine();
+        }
+
+        int age;
+        string name;
+        char firstLetterOfName;
+        bool canJump;
+        float weight;
+        double height;
+        long ageOfEarth;
+        short populationOfUglich;
+        ushort populationOfStupino;
+        byte putinsAge;
+    }
+}
---
+++ CSLight/ОП_Задание1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class ОП_Задание1
+    {
+        int age;
+        string name;
+        char firstLetterOfName;
+        bool canJump;
+        float weight;
+        double height;
+        long ageOfEarth;
+        short populationOfUglich;
+        ushort populationOfStupino;
+        byte putinsAge;
+    }
+}
